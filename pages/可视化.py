2620b6ebb356aa00f47edf05935252763b006ff5
coyomii <2995265803@qq.com>
2024-11-28 11:56:17 +0800

    更新字体设置以注册新的全局字体 SimHei，确保在 Linux 中正常显示中文标签。

--- pages/可视化.py
+++ pages/可视化.py
@@ -5,10 +5,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import streamlit as st
+from matplotlib import font_manager
 
 from utils.common_utils import *
 
-plt.rcParams['font.sans-serif']=['Microsoft YaHei'] # 测试Linux正常显示中文标签
+# 临时注册新的全局字体
+font_manager.fontManager.addfont(r'fonts\SimHei.ttf')
+plt.rcParams['font.sans-serif']=['SimHei'] # 测试Linux正常显示中文标签
 plt.rcParams['axes.unicode_minus']=False # 正常显示负号
 
 
